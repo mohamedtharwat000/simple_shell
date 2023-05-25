87970ede3f6f70004e993a3e55423d7f60a3db69
Alialsayad <20508000a@gmail.com>
2023-05-25 07:48:20 -0700

    a

--- main.h
+++ main.h
@@ -5,8 +5,13 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <errno.h>
+#include <string.h>
+#include <limits.h>
+#include <fcntl.h>
+
 
 #define BUFFSIZE 1024
 extern char **environ;

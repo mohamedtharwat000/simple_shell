06ef244f9e2d28124bda1ba956ec90c15f84a63e
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-05-26 18:52:58 +0300

    test

---
+++ shell_getline.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+/**
+ * _getline -  GEts inputs.
+ * Return: The input.
+ */
+
+char *_getline(void)
+{
+	char *lineptr = NULL;
+	size_t charter_user = 0;
+
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "$ ", 2);
+
+	if (getline(&lineptr, &charter_user, stdin) == -1)
+	{
+		free(lineptr);
+		return (NULL);
+	}
+
+	return (lineptr);
+}
